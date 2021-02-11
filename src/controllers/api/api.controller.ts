cb86eccf5d0651fa13d3560e668baea9756cd792
Stanisław Uss <uss.stanislaw@protonmail.com>
2021-02-11 14:36:13 +0100

    Aktywności

--- src/controllers/api/api.controller.ts
+++ src/controllers/api/api.controller.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import ActivityTypesController from './activityTypes/activityTypes.controller';
 import ClassesController from './classes/classes.controller';
 import UsersController from './users/users.controller';
+import ActivityLogsController from './activityLogs/activityLogs.controller';
 
 class ApiController implements Controller {
   public path = ['/api', '/api/v1'];
@@ -10,11 +11,13 @@ class ApiController implements Controller {
   private activityTypesController: ActivityTypesController;
   private classesController: ClassesController;
   private usersController: UsersController;
+  private activityLogsController: ActivityLogsController;
 
   constructor() {
     this.activityTypesController = new ActivityTypesController();
     this.classesController = new ClassesController();
     this.usersController = new UsersController();
+    this.activityLogsController = new ActivityLogsController();
     this.initializeSubControllers();
   }
 
@@ -23,6 +26,7 @@ class ApiController implements Controller {
       this.router.use(path, this.activityTypesController.router);
       this.router.use(path, this.classesController.router);
       this.router.use(path, this.usersController.router);
+      this.router.use(path, this.activityLogsController.router);
     });
   }
 }
