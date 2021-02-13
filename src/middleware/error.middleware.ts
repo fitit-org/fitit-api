2845cac113e529982d4aeda5da7c16dcc805dde9
Stanisław Uss <uss.stanislaw@protonmail.com>
2021-02-13 18:59:18 +0100

    Poprawka w error-handlingu i w init-skrypcie

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -1,10 +1,14 @@
-import { Request, Response } from 'express';
+// NextFunction has to be provided (despite not being used) so that the middleware is recognized by express as error-handling
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import { Request, Response, NextFunction } from 'express';
 import HttpException from '../exceptions/HttpException';
 
 function errorMiddleware(
   error: HttpException,
   request: Request,
-  response: Response
+  response: Response,
+  next: NextFunction
 ): void {
   const status = error.status || 500;
   const message = error.message || 'Something went wrong';
